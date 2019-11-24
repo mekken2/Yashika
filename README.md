07ad1c5a24152d0616be3e1886ac8b99f5c1ef05
Priyanshu <priyanshukumar9431@gmail.com>
2019-11-25 03:21:17 +0530

    Added jarvis.py, README.md and LICENSE.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# JARVIS
+ This is a try to copy the Jarvis of Iron Man (Assisstant of Tony Stark)
+
+## Steps to Run JARVIS
+1. Clone the repository
+2. Run jarvis.py using cmd/terminal
+
+## Things it can do now
+1. Open Websites
+2. Search Wikipedia
+3. Send Emails
+4. Open files
+5. Play music and videos
+
+**_You may follow me and star this repo to contribute in my motivation for building more projects_**
+
+## Tips
+1. Edit the music directory to your own directory path
+2. Edit the Email ID to your own email ID and real password (You may encrypt it by using some encryption techniques)
+
+## TODO
+- [ ] Make Advancements in opening websites
+- [ ] Increase efficiency in searching data on Wikipedia
+
+**You may modify it as you like and can even open a PR anytime about amazing thoughts and ideas**
---
+++ jarvis.py
@@ -0,0 +1,111 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import random
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+
+    speak('I am Jarvis(developed by Priyanshu). How may I help you?')
+
+#Takes Microphone input from User and Enters String value
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 0.8
+        r.energy_threshold = 400
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print("User said:", query)
+
+    except Exception as e:
+        print(e)
+
+        print("Say that again please...")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP(smtp.gmail.com, 587)
+    server.ehlo()
+    server.starttls()
+    server.login('YOUR_EMAIL_ID@gmail.com', 'YOUR_PASSWORD')
+    server.sendmail('YOUR_EMAIL_ID@gmail.com',to,content)
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        #Logic for executing tasks based on query
+
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            speak("Opening YouTube for master Prince")
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            speak("Opening Google for master Prince")
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            speak("Opening Stack Overflow for master Prince")
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            speak("Playing music for master Prince")
+            music_dir = "YOUR_MUSIC_DIRECTORY"
+            songs = os.listdir(music_dir)
+            print(songs)
+            num = int(random.randint(0,380))
+            os.startfile(os.path.join(music_dir, songs[num]))
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak("The time is " + strTime)
+
+        elif 'editor' in query:
+            speak("Opening Sublime Text")
+            path = "C:\\Program Files\\Sublime Text 3\\sublime_text.exe"
+            os.startfile(path)
+
+        elif 'email to Prince' in query:
+            try:
+                speak("What should I send?")
+                content = takeCommand()
+                to = "emailtosend@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry, I am unable to send Email at the moment")
+
